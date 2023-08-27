3a0af9de823b0e090c7a0433b315dd45e06cbfb4
subburam02 <subburam002@gmail.com>
2023-08-27 12:44:28 +0530

    First commit

--- src/components/Headerlogo.jsx
+++ src/components/Headerlogo.jsx
@@ -1,11 +1,10 @@
 import React from "react";
 import ReactDOM  from "react-dom";
-import Logo from "../assets/tvs-logo.png";
 function Headerlogo()
 {
     return (
          <nav className="navbar navbar-expand-sm navbar-toggleable-sm navbar-dark bg-primary" alt="logo">
-        <img src={Logo} style={{marginRight:'400px'}}/> 
+        <img alt="Logo" style={{marginRight:'400px'}}/> 
     
         <h1 style={{justifyContent:"center"}}> Employee Leaderboard </h1>
         </nav>
